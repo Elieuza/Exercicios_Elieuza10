41c97084d7193cf48b959366aebc5d1caf4de01b
Elieuza <lilaspada@yahoo.com.br>
2021-01-28 18:26:38 -0300

    Atividade Criada

---
+++ Atividade/Atividade/Form2.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Atividade
+{
+    partial class frmParImpar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dígite um número";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(135, 37);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 20);
+            this.txtNum.TabIndex = 1;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(311, 34);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(118, 23);
+            this.btnVerificar.TabIndex = 2;
+            this.btnVerificar.Text = "Verificar números";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // frmParImpar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 134);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label1);
+            this.Name = "frmParImpar";
+            this.Text = "Verificar número Par ou Impar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
---
+++ Atividade/Atividade/Form4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade
+{
+    public partial class frmReajuste : Form
+    {
+        public frmReajuste()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReajuste_Click(object sender, EventArgs e)
+        {
+            double salario = Convert.ToDouble(txtSalario.Text);
+            double salarioAjustado = 0;
+
+            if(salario <= 300)
+            {
+                salarioAjustado = salario+(salario/2);
+                MessageBox.Show(Convert.ToString(salarioAjustado));
+            }
+            else
+            {
+                salarioAjustado = salario + ((salario * 30)/100);
+                MessageBox.Show(Convert.ToString(salarioAjustado));
+            }
+        }
+    }
+}
